dca49b3d746ed37c35dea7baeae61c11be4ff168
Billpzt <billpiazzetta@gmail.com>
2023-05-18 12:11:13 -0300

    Uso de API com fetch

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,15 +1,25 @@
 import { Text, StyleSheet, View, Button, FlatList } from 'react-native';
 import  ProdutoCard from '../components/ProdutoCard'
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
 
-    const produtos = [
-        {nome: 'Mouse', preco: 53.78, quant: 1, path: 'https://images.pexels.com/photos/2115256/pexels-photo-2115256.jpeg'},
-        {nome: 'Teclado', preco: 153.78, quant: 1, path: 'https://images.pexels.com/photos/841228/pexels-photo-841228.jpeg'},
-        {nome: 'Monitor', preco: 253.78, quant: 1, path: 'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg'},
-        {nome: 'Celular', preco: 553.78, quant: 1, path: 'https://images.pexels.com/photos/607812/pexels-photo-607812.jpeg'},
-    ];
+    // const produtos = [
+    //     {nome: 'Mouse', preco: 53.78, quant: 1, path: 'https://images.pexels.com/photos/2115256/pexels-photo-2115256.jpeg'},
+    //     {nome: 'Teclado', preco: 153.78, quant: 1, path: 'https://images.pexels.com/photos/841228/pexels-photo-841228.jpeg'},
+    //     {nome: 'Monitor', preco: 253.78, quant: 1, path: 'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg'},
+    //     {nome: 'Celular', preco: 553.78, quant: 1, path: 'https://images.pexels.com/photos/607812/pexels-photo-607812.jpeg'},
+    // ];
 
+    const [produtos, setProdutos] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/produtos")
+        .then((res) => res.json())
+        .then(data => setProdutos(data));
+    }, [])
+
+    
     const mapProdutos = produtos
         .map((prod, index) => ({
             key: index, value: prod
@@ -18,9 +28,9 @@ export default function Home() {
     const renderItem = ({item}) => (
         <ProdutoCard 
             path={item.value.path}
-            nome={item.value.nome}
-            preco={item.value.preco}
-            quantidade={item.value.quant}
+            nome={item.value.name}
+            preco={item.value.id}
+            quantidade={item.value.id}
         />
     );
     
