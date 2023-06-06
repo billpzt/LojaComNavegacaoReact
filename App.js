d1c704ccedf4139fceb65d3049da1a713ffc2e02
Billpzt <billpiazzetta@gmail.com>
2023-06-06 10:56:31 -0300

    Tentei introduzir a drawer navigation

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import { StackActions } from '@react-navigation/native'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './views/Home'
 import About from './views/About'
 import Detalhes from './views/Detalhes'
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Detalhes" component={Detalhes} />
+        <Drawer.Screen name="Sobre" component={About} />
+      </Drawer.Navigator>
+      {/* <Stack.Navigator>
         <Stack.Screen
           name='Home'
           component={Home}
@@ -24,7 +31,7 @@ export default function App() {
           name='About'
           component={About}
         />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
     </NavigationContainer>
 )}
 
